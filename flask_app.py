e0ab99038250bc2218ce2545751f31b5de5bbf81
Sahith Dambekodi <sahithdn@gmail.com>
2017-08-18 00:16:44 +0530

    Parsing happens dynamically at runtime using a single html page

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,27 @@
 from flask import Flask, request, session, redirect, url_for, \
                   send_from_directory, render_template
 
+from parser import make_file
+from transitions import make_option_files
+
 app = Flask(__name__)
 
 @app.route('/')
 def Start():
-    return render_template('page.html')
+	make_option_files("Start")
+	make_file("Start")
+	return render_template('page.html')
 
 @app.route('/next', methods =['POST'])
 def  next():
-    if request.method == 'POST':
-        form = request.form['option']
-       	# print(next_node)
-        return redirect(url_for('options',next_node=form))
+	if request.method == 'POST':
+		form = request.form['option']
+		print(form)
+		make_option_files(form)
+		make_file(form)
+
+		return render_template('page.html')
+        # return redirect(url_for('options',next_node=form))
 
 @app.route('/options/<next_node>')
 def options(next_node):
